62e09085f4b69845eaf6ac0acfb8894bec0c5276
Trang <trangnn@magenest.com>
2019-11-07 10:07:18 +0700

    customize share pluggin flutter

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\flutter_windows_v1.9.1+hotfix.6-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_windows_v1.9.1+hotfix.6-stable\flutter\.pub-cache\hosted\pub.dartlang.org\share-0.6.3\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\flutter_windows_v1.9.1+hotfix.6-stable\flutter\bin\cache\artifacts\engine\ios"
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <e2e/E2EPlugin.h>
+#import <share/SharePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
+  [FLTSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharePlugin"]];
+}
+
+@end
